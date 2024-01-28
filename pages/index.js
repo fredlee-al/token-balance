1a882ab21b33881f0724cb6a3eefd505bcaeebb9
Fred Lee <128230178+fredlee-al@users.noreply.github.com>
2024-01-28 08:54:06 +0530

    index.js

--- pages/index.js
+++ pages/index.js
@@ -253,7 +253,7 @@ export default function Home() {
           <div className="p-4 text-center">
             <div>
               <a href="https://www.github.com" className="text-blue-500">
-               Made by Fred at UDYAMITSAV'24 
+               Made at UDYAMITSAV'24 
               </a>
               <br />
             </div>
